1039db025d3d93dd90b160935192dd45146f2ae8
omarwebdeve <omarf977865@gmail.com>
2023-02-17 19:49:48 +0600

    dashboard home -omar

---
+++ src/Pages/Dashboard/DashboardHome.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const DashboardHome = () => {
+  return (
+    <div className="flex items-center h-[80vh] justify-center">
+      <div className="max-w-3xl">
+        <h2 className="text-center text-2xl lg:text-5xl font-sans font-semibold">
+          Admin Dashboard for updating contents, categories, colors
+        </h2>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardHome;
